6f912d3d7825ca9262ea31fcc0d7e5cdca023c88
Javier Parada Tallante <javierparada73@outlook.com>
2019-04-12 16:51:16 +0200

    chenge select view from hiragana to kataklana

--- app/src/main/java/me/kabi404/mikana/SelectActivity.java
+++ app/src/main/java/me/kabi404/mikana/SelectActivity.java
@@ -1,16 +1,24 @@
 package me.kabi404.mikana;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.RadioButton;
 
 import me.kabi404.mikana.adapter.KanaRowsAdapter;
+import me.kabi404.mikana.model.KanaManager;
+import me.kabi404.mikana.model.Syllabary;
 
 public class SelectActivity extends AppCompatActivity {
+
+    private ListView kanaRows;
+    private RadioButton hiraganaRadio, katakanaRadio;
+    KanaRowsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +27,23 @@ public class SelectActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ListView lv = (ListView) findViewById(R.id.kanaRows);
+        kanaRows = (ListView) findViewById(R.id.kanaRows);
+        adapter = new KanaRowsAdapter(this);
+        kanaRows.setAdapter(adapter);
 
-        KanaRowsAdapter adapter = new KanaRowsAdapter(this);
+        hiraganaRadio = (RadioButton) findViewById(R.id.hiraganaRadio);
+        katakanaRadio = (RadioButton) findViewById(R.id.katakanaRadio);
 
-        lv.setAdapter(adapter);
+    }
 
+    public void setToHiragana(View view) {
+        KanaManager.getInstance().setCurrentSyllabary(Syllabary.HIRAGANA);
+        adapter.notifyDataSetChanged();
+    }
+
+    public void setToKatakana(View view) {
+        KanaManager.getInstance().setCurrentSyllabary(Syllabary.KATAKANA);
+        adapter.notifyDataSetChanged();
     }
 
 }
