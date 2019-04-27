98744ff937604dca1c2a842536c889f196f3263d
Javier Parada Tallante <javierparada73@outlook.com>
2019-04-27 12:14:49 +0200

    update app domain

--- app/src/androidTest/java/com/artichokecore/mikana/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/artichokecore/mikana/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("me.kabi404.mikana", appContext.getPackageName());
+        assertEquals("com.artichokecore.mikana", appContext.getPackageName());
     }
 }
