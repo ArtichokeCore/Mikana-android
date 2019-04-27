5e6377620755207630495792b3fa9b3753b56304
mpardalm <mpardalm@outlook.com>
2019-04-27 11:41:18 +0200

    Fix #10
    Need a better image for Splash

--- app/src/main/java/com/artichokecore/mikana/activities/MainActivity.java
+++ app/src/main/java/com/artichokecore/mikana/activities/MainActivity.java
@@ -30,6 +30,7 @@ public final class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.SplashTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
